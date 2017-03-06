07d6f1d557e32905666f677b2e1cf312fee4bd0d
kanghonggu <wckhg89@gmail.com>
2017-03-06 14:52:22 +0900

    interface

---
+++ src/interface/interface-implements.ts
@@ -0,0 +1,26 @@
+interface Animals {
+  leg: number;
+}
+
+interface Bird extends Animals {
+  wing: number;
+  getNumWing();
+}
+
+class BlueBird implements Bird {
+  wing: number;
+  leg: number;
+
+  constructor(leg:number, wing:number) {
+    this.leg = leg;
+    this.wing = wing;
+  }
+
+  getNumWing () {
+    return this.wing;
+  }
+
+}
+
+let myBlueBird = new BlueBird(2,2);
+console.log("파랑새의 날개 : "+myBlueBird.getNumWing()+"개");
